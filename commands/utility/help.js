e2ae93c5f0a5ed18ac55c15c20d5dd158de53ed5
Geborgen <geborgen3000@gmail.com>
2024-03-28 15:48:03 -0400

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -9,7 +9,7 @@ module.exports = {
         const helpEmbed = new EmbedBuilder()
             .setColor(0x2e0303)
             .setTitle('Contest Commands')
-            .setDescription('/addpoints [user] [points] [reason]\n- Adds points to either Jack or Jake\n- If another user is entered, will display but does not store data\n- Admin only\n\n/removepoints [user] [points] [reason]\n- Removes points from either Jack or Jake\n- If another user is entered, will display but does not store data\n- Admin only\n\n/showscore\n- Shows total score\n- Anyone can use\n\n/ping\n- Returns bot latency\n- Anyone can use\n\n/help\n- Shows this, you fucking idiot\n- Anyone can use');
+            .setDescription('/addpoints [user] [points] [reason]\n- Adds points to either Jack or Jake\n- If another user is entered, will display but does not store data\n- Admin only\n\n/removepoints [user] [points] [reason]\n- Removes points from either Jack or Jake\n- If another user is entered, will display but does not store data\n- Admin only\n\n/showscore\n- Shows total score\n- Anyone can use\n\n/ping\n- Returns bot latency\n- Anyone can use\n\n/help\n- Shows this\n- Anyone can use');
 		await interaction.reply({ embeds: [helpEmbed] });
 	},
 };
