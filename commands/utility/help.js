80179462d83cd8dba2532d2a6d62b862b3f0e501
Geborgen <geborgen3000@gmail.com>
2024-03-28 16:04:18 -0400

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setName('help')
 		.setDescription('Shows all commands and uses'),
 	async execute(interaction) {
-        const helpEmbed = new EmbedBuilder()
+    	const helpEmbed = new EmbedBuilder()
         	.setColor(0x2e0303)
         	.setTitle('Contest Commands')
         	.setDescription('/addpoints [user] [points] [reason]\n- Adds points to either Jack or Jake\n- If another user is entered, will display but does not store data\n- Admin only\n\n/removepoints [user] [points] [reason]\n- Removes points from either Jack or Jake\n- If another user is entered, will display but does not store data\n- Admin only\n\n/showscore\n- Shows total score\n- Anyone can use\n\n/ping\n- Returns bot latency\n- Anyone can use\n\n/help\n- Shows this\n- Anyone can use');
