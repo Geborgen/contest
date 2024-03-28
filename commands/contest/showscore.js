72fff504a8e75becc6c0c0714cabe67bd0e46ffc
Geborgen <geborgen3000@gmail.com>
2024-03-28 15:54:08 -0400

    Update showscore.js

--- commands/contest/showscore.js
+++ commands/contest/showscore.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('showscore')
 		.setDescription('Shows total score'),
 	async execute(interaction) {
-            var points = require('../../points.json');
-            await interaction.reply('**Team Prack:** Jack has ' + points.jackPoints + ' point(s). \n**Team Prake:** Jake has ' + points.jakePoints + ' point(s).');
+        var points = require('../../points.json');
+        await interaction.reply('**Team Prack:** Jack has ' + points.jackPoints + ' point(s). \n**Team Prake:** Jake has ' + points.jakePoints + ' point(s).');
 	},
 };
